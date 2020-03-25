cd007c75a615f0b37102029df9eed297afc06db2
Artsiom <sorintema@gmail.com>
2020-03-25 18:15:18 +0300

    MADE PICTURES AND BLOGS UUUHHHHHHHUUUU

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,6 @@ from .models import BlogArticle
 
 def all_blogs(request):
     articles=BlogArticle.objects.all()
-    return render(request, 'blog/blogs.html',{'articles':articles})
+    return render(request, 'blog/blogs.html', {'articles' : articles})
 
 # Create your views here.

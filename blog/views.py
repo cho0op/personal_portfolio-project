a134fd54410557f824de5d882cf08bc239c34e64
Artsiom <sorintema@gmail.com>
2020-03-26 14:12:36 +0300

    added links to blogs articles

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import BlogArticle
 
 def all_blogs(request):
     articles=BlogArticle.objects.all()
     return render(request, 'blog/blogs.html', {'articles' : articles})
 
+def detail(request, article_id):
+    article = get_object_or_404(BlogArticle, pk=article_id)
+    return render(request, 'blog/detail.html',{'article': article})
 # Create your views here.

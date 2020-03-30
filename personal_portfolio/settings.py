bf3042d2f0fcc0eecd8f733769ced47dc13ba767
Artsiom <sorintema@gmail.com>
2020-03-30 20:03:26 +0300

    fixed details for server

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -126,3 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+try:
+    from .local_settings import *
+except:
+    print("no local settings")
